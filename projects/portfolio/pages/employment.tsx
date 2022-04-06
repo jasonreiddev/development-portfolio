977319681a4bf35bf72269f09ca4e535dd397da9
Jason Reid <jasonreid@netconstruct.co.uk>
2022-04-06 20:04:17 +0100

    getStaticProps for getting data with SSR

--- projects/portfolio/pages/employment.tsx
+++ projects/portfolio/pages/employment.tsx
@@ -1,69 +1,71 @@
-import type { NextPage } from 'next';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { LayoutContext } from '../../helpers/layoutContext';
 import { client } from '../../helpers/client';
 import { CardTimeline } from '../../component-library/stories/components/CardTimeline/CardTimeline';
 import { CardProps } from '../../component-library/stories/components/Card/Card';
 
-const Employment: NextPage = () => {
+type EmploymentsProps = { data: any };
+
+export const getStaticProps = async () => {
+  const res = await client.fetch(
+    `
+    *[_type == "position"]
+  `,
+  );
+  const data = await res;
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const Employment = ({ data }: EmploymentsProps) => {
   const { updatePageTitle } = useContext(LayoutContext);
   updatePageTitle && updatePageTitle('Employment');
 
-  const [cards, setCards] = useState<CardProps[]>([]);
-  useEffect(() => {
-    const fetchPositions = async (): Promise<any> => {
-      const promise = await client.fetch(
-        `
-        *[_type == "position"]
-      `,
-      );
-      const response = await promise;
-      const Positions: CardProps[] = [];
-      response.sort(function (a: any, b: any) {
-        // by desc endDate // TODO add start date
-        a.endDate = a.endDate == null ? Date.now() : a.endDate;
-        b.endDate = b.endDate == null ? Date.now() : b.endDate;
-        return new Date(b.endDate).getTime() - new Date(a.endDate).getTime();
+  const Positions: CardProps[] = [];
+  data.sort(function (a: any, b: any) {
+    // by desc endDate // TODO add start date
+    a.endDate = a.endDate == null ? Date.now() : a.endDate;
+    b.endDate = b.endDate == null ? Date.now() : b.endDate;
+    return new Date(b.endDate).getTime() - new Date(a.endDate).getTime();
+  });
+  // TODO Calculate Length
+  data.map((position: any) => {
+    if (!position.hideOnEmployment) {
+      position.startDate = new Date(position.endDate).toLocaleDateString('en-GB', {
+        month: 'long',
+        year: 'numeric',
       });
-      // TODO Calculate Length
-      response.map((position: any) => {
-        if (!position.hideOnEmployment) {
-          position.startDate = new Date(position.endDate).toLocaleDateString('en-GB', {
-            month: 'long',
-            year: 'numeric',
-          });
-          position.endDate =
-            position.endDate == null
-              ? `Present `
-              : `${new Date(position.endDate).toLocaleDateString('en-GB', {
-                  month: 'long',
-                  year: 'numeric',
-                })}.`;
-          Positions.push({
-            title: position.jobTitle,
-            flipContent: (
-              <>
-                {/* TODO Include organisation info */}
-                {/* <h3>{`${position.jobTitle} | ${position.organisation.organisation}`}</h3> */}
-                <h3>{position.jobTitle}</h3>
-                <p>
-                  {position.startDate} - {position.endDate}
-                </p>
-                <hr />
-                <br />
-                <p>{position.details}</p>
-              </>
-            ),
-            text: `${position.startDate} - ${position.endDate}\n${position.details}`,
-          });
-        }
+      position.endDate =
+        position.endDate == null
+          ? `Present `
+          : `${new Date(position.endDate).toLocaleDateString('en-GB', {
+              month: 'long',
+              year: 'numeric',
+            })}.`;
+      Positions.push({
+        title: position.jobTitle,
+        flipContent: (
+          <>
+            {/* TODO Include organisation info */}
+            {/* <h3>{`${position.jobTitle} | ${position.organisation.organisation}`}</h3> */}
+            <h3>{position.jobTitle}</h3>
+            <p>
+              {position.startDate} - {position.endDate}
+            </p>
+            <hr />
+            <br />
+            <p>{position.details}</p>
+          </>
+        ),
+        text: `${position.startDate} - ${position.endDate}\n${position.details}`,
       });
-      setCards([...Positions]);
-    };
-    fetchPositions();
-  }, []);
-
-  return <>{cards.length > 0 && <CardTimeline cards={cards} size={300} />}</>;
+    }
+  });
+  return <>{Positions.length > 0 && <CardTimeline cards={Positions} size={300} />}</>;
 };
 
 export default Employment;
