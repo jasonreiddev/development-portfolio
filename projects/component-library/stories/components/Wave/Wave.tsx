a442eaaa3a63dbbd74f01a6f950f7e41fea6a004
Jason Reid <jasonreid@netconstruct.co.uk>
2022-04-15 00:21:41 +0100

    Wave component

---
+++ projects/component-library/stories/components/Wave/Wave.tsx
@@ -0,0 +1,10 @@
+import { MdFrontHand, MdInfo } from 'react-icons/md';
+import { WaveStyles as s } from './Wave.styles';
+
+export const Wave = (): JSX.Element => {
+  return (
+    <s.Wrapper>
+      <MdFrontHand />
+    </s.Wrapper>
+  );
+};
