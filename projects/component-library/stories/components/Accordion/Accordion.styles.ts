74d4e4f9159f511cf7f47c8e55050d84bbbccc56
Jason Reid <jasonreid@netconstruct.co.uk>
2022-05-17 23:40:36 +0100

    Add color pair to accordion - example

--- projects/component-library/stories/components/Accordion/Accordion.styles.ts
+++ projects/component-library/stories/components/Accordion/Accordion.styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+import { ColorPair, useColorPair } from '../../../../helpers/media';
+
+interface AccordionStylesProps {
+  active?: boolean;
+  colorPair?: ColorPair;
+}
+
 const Wrapper = styled.div`
   font-size: 1rem;
   color: hsl(0deg 0% 25%);
@@ -10,7 +17,7 @@ const Wrapper = styled.div`
   border-radius: var(--border-radius);
 `;
 
-const Title = styled.button`
+const Title = styled.button<AccordionStylesProps>`
   color: var(--color-contrast);
   cursor: pointer;
   padding: 12px 16px;
@@ -21,15 +28,17 @@ const Title = styled.button`
   border-radius: var(--border-radius);
   font-weight: bold;
   font-size: 14pt;
+
+  ${(p: AccordionStylesProps) => useColorPair(p.colorPair, '10pt')}
 `;
 
-const Content = styled.div<{ active?: boolean }>`
-  background-color: var(--color-secondary-ld-max);
+const Content = styled.div<AccordionStylesProps>`
   border-radius: 0 0 var(--border-radius) var(--border-radius);
   padding: 26px 8px 8px;
   margin: -16px 0 0;
 
   ${(p) => (p.active ? ` display: block;` : `  display: none;`)}
+  ${(p: AccordionStylesProps) => useColorPair(p.colorPair, '10pt')}
 `;
 
 export const AccordionStyles = {
